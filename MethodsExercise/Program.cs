6bdb4b17dbf93309d98d60243bba7e2d44015714
Joseph Tracy <JTracy87@gmail.com>
2023-01-19 23:16:00 -0800

    first step of exercise

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            Console.WriteLine("What is your first name?");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine($"Hi, {firstName}, what is your favorite color?");
+            var favoriteColor = Console.ReadLine();
+
+            Console.WriteLine($"I like {favoriteColor} too. What is your favorite animal?");
+            var favoriteAnimal = Console.ReadLine();
+
+            Console.WriteLine($"{favoriteAnimal}s are great. What is your favorite band?");
+            var favoriteBand = Console.ReadLine();
+
         }
     }
 }
